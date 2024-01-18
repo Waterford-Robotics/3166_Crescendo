69d9a0632e96024d750513d05095d8390184a6a6
CyberLord0001 <lucashshen@gmail.com>
2024-01-17 17:11:11 -0700

    Hihihihihi

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,7 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-
+//Lucas was here
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
