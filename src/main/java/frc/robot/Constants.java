48649fe89329c8b213c6bb90bf022a2e595a5c10
Ivan Post <ivan.post24@gmail.com>
2024-01-20 21:20:12 -0700

    Scaffolding for time-based code

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+public final class Constants {
+
+  // Constants go here...
+
+  // This class cannot be initalized.
+  private Constants() {}
+}
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,5 +1,12 @@
 package frc.robot.subsystems;
 
+/**
+ * The robot's drivetrain.
+ */
 public final class Drivetrain {
+  
+  // Instance variables go here...
+
+  // Methods go here...
   
 }
