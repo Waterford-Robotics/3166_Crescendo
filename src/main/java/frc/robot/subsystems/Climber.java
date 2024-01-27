38492748d5f499bc01816588b8966f35f08f5565
Jet Webster <jetwebster@students.waterfordschool.org>
2024-01-27 10:45:15 -0700

    added climb method.             stop method.             descend method.

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,6 +4,19 @@ import edu.wpi.first.wpilibj.motorcontrol.Talon;
 
 public final class Climber {
     
-    Talon m_lcmotor1 = new Talon(6);
-    Talon m_rcmotor2 = new Talon(7);
+    Talon m_lcmotor = new Talon(6);
+    Talon m_rcmotor = new Talon(7);
+
+    public void climb(){
+        m_lcmotor.set(.7);
+        m_rcmotor.set(.7);
+    } 
+    public void stop(){
+        m_lcmotor.set(0);
+        m_rcmotor.set(0);
+    }
+    public void descend(){
+        m_lcmotor.set(-.7);
+        m_rcmotor.set(-.7);
+    }
 }
