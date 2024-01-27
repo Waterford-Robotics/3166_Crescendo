641bb3b02c03f12390f277bbee27f1d5e5ef53f8
Nitya Sunkara <nityasunkara@students.waterfordschool.org>
2024-01-26 17:08:34 -0700

    add motor controllers

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,11 +1,20 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+
 /**
  * The robot's drivetrain.
  */
 public final class Drivetrain {
   
-  // Instance variables go here...
+  
+  private Talon m_right1 = new Talon(0);
+  private Talon m_right2 = new Talon(1);
+  private Talon m_left1 = new Talon(2);
+  private Talon m_left2 = new Talon(3);
+  private MotorControllerGroup m_rightMotors = new MotorControllerGroup(m_right1, m_right2);
+  private MotorControllerGroup m_leftMotors = new MotorControllerGroup(m_left1, m_left2);
 
   // Methods go here...
   
