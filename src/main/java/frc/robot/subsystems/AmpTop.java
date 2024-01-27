41e0ee0f8eb7507d5c730fdf2a3f812417ce5848
Jet Webster <jetwebster@students.waterfordschool.org>
2024-01-27 11:10:52 -0700

    made talon motors for amp + methods for amp

--- src/main/java/frc/robot/subsystems/AmpTop.java
+++ src/main/java/frc/robot/subsystems/AmpTop.java
@@ -1,5 +1,17 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+
 public final class AmpTop {
+    Talon m_beltmotor = new Talon(0);
+    Talon m_shaftmotor = new Talon(0);
   
+    public void shoot (){
+      m_beltmotor.set(.7);
+      m_shaftmotor.set(.7);
+    }
+    public void shootfinish (){
+      m_beltmotor.set(0);
+      m_shaftmotor.set(0);
+    }
 }
