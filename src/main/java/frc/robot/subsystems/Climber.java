1e42272a17f7cf08119c85fa7fb9f9318e7b279f
Jet Webster <jetwebster@students.waterfordschool.org>
2024-01-27 10:25:56 -0700

    talon motors implemented (ports: 6 & 7)

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+
 public final class Climber {
-  
+    
+    Talon m_lcmotor1 = new Talon(6);
+    Talon m_rcmotor2 = new Talon(7);
 }
