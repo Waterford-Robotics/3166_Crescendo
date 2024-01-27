1005e616202fc382e903e66d549183a6794b44ef
Jet Webster <jetwebster@students.waterfordschool.org>
2024-01-27 11:05:54 -0700

    made methods for shooting and stop shooting in amp top

--- src/main/java/frc/robot/subsystems/AmpTop.java
+++ src/main/java/frc/robot/subsystems/AmpTop.java
@@ -5,4 +5,13 @@ import edu.wpi.first.wpilibj.motorcontrol.Talon;
 public final class AmpTop {
   Talon m_beltmotor = new Talon(0);
   Talon m_shaftmotor = new Talon(0);
+
+  public void shoot (){
+    m_beltmotor.set(.7);
+    m_shaftmotor.set(.7);
+  }
+  public void shootfinish (){
+    m_beltmotor.set(0);
+    m_shaftmotor.set(0);
+  }
 }
