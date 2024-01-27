5f5e278675e579d4215a0f235e621c1d7672740b
Jet Webster <jetwebster@students.waterfordschool.org>
2024-01-27 10:58:59 -0700

    ampTop initialized belt motor and shaft motor.

--- src/main/java/frc/robot/subsystems/AmpTop.java
+++ src/main/java/frc/robot/subsystems/AmpTop.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+
 public final class AmpTop {
-  
+  Talon m_beltmotor = new Talon(0);
+  Talon m_shaftmotor = new Talon(0);
 }
