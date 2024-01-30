2d37f5d76a7cdfe84e3df50554b683a7ec297a49
Jet Webster <jetwebster@students.waterfordschool.org>
2024-01-29 17:16:38 -0700

    Add reverse method + extra motor + Constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,7 +3,8 @@ package frc.robot;
 public final class Constants {
 
   // Constants go here...
-  public static final int kShootmotorID = 8;
+  public static final int kShootmotorID = 9;
+  public static final int kDoubleShootmotorID =8;
   public static final double kShootSpeed = .8;
   public static final int kControllerID = 0;
   public static final int kShootButtonID = 3;
--- src/main/java/frc/robot/subsystems/SpeakerTop.java
+++ src/main/java/frc/robot/subsystems/SpeakerTop.java
@@ -4,11 +4,19 @@ import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import frc.robot.Constants;
 
 public final class SpeakerTop {
+    private Talon m_doubleshootMotor = new Talon(Constants.kDoubleShootmotorID);
     private Talon m_shootMotor = new Talon(Constants.kShootmotorID);
+
     public void shoot (){
         m_shootMotor.set(Constants.kShootSpeed);
+        m_doubleshootMotor.set(Constants.kShootSpeed);
     }
     public void stop (){
         m_shootMotor.set(0);
+        m_doubleshootMotor.set(0);
+    }
+    public void reverse (){
+        m_shootMotor.set(-Constants.kShootSpeed);
+        m_doubleshootMotor.set(-Constants.kShootSpeed);
     }
 }
