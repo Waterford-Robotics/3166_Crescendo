61b4aef1cce370825381867a46ade3ab8c9f8223
CyberLord0001 <lucashshen@gmail.com>
2024-01-31 16:03:26 -0700

    Replaced defaultCommand with finallyDo and added constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController.Button;
+
 public final class Constants {
 
   // Constants go here. It's standard to separate them into static inner classes. An example is created below.
@@ -12,6 +14,7 @@ public final class Constants {
   }
   public static final class OIConstants {
     public static final int kDriverControllerPort = 0;
+    public static final int kBButton = Button.kB.value;
   }
   // This class cannot be initialized.
   private Constants() {}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -20,17 +19,16 @@ public class RobotContainer {
   // Instance variables go here. This typically includes all robot systems and controllers.
 
   public RobotContainer() {
-    m_ampShooter.setDefaultCommand(
-        new RunCommand(() -> m_ampShooter.stop(), m_ampShooter)
-    );
     configureBindings();
   }
 
   private void configureBindings() {
-    new JoystickButton(m_driverController, Button.kB.value)
+    new JoystickButton(m_driverController, OIConstants.kBButton)
     .whileTrue(new RunCommand(
-        () -> m_ampShooter.shoot(), 
-        m_ampShooter));
+      () -> m_ampShooter.shoot(), 
+      m_ampShooter).finallyDo((interrupted) -> {
+        m_ampShooter.stop();
+      }));
   }
 
   public Command getAutonomousCommand() {
