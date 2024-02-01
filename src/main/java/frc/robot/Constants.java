0e2efbcb460ee9b7ef7f8941091aa3e606b6ec98
Jet Webster <jetwebster@students.waterfordschool.org>
2024-02-01 14:44:14 -0700

    fixed requests

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,7 +5,9 @@ public final class Constants {
   // Constants go here...
   public static final int kControllerPortID = 0;
   public static final int kShootButtonID = 2;
+
   public static final double kShootSpeed = .7;
+  
   public static final int kShaftmotorID = 6;
   public static final int kBeltmotorID = 7;
 
