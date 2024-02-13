2a299eb05c31ebdbcc87c3714377baabc7ba570e
CyberLord0001 <lucashshen@gmail.com>
2024-02-12 18:27:35 -0700

    Merged to 2024

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,29 +4,28 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimberConstants;
 
 public class Climber extends SubsystemBase {
-  private WPI_TalonFX m_climbMotor1 = new WPI_TalonFX(ClimberConstants.kClimberID1);
-  private WPI_TalonFX m_climbMotor2 = new WPI_TalonFX(ClimberConstants.kClimberID2);
-  private MotorControllerGroup m_climbMotors = new MotorControllerGroup(m_climbMotor1, m_climbMotor2);
-  public Climber() {
-    m_climbMotor2.setInverted(true);
-  }
+  private TalonFX m_climbMotor1 = new TalonFX(ClimberConstants.kClimberID1);
+  private TalonFX m_climbMotor2 = new TalonFX(ClimberConstants.kClimberID2);
+  private Follower m_climbMotor2Follower = new Follower(ClimberConstants.kClimberID1, true);
+  public Climber() {}
 
   public void climb(){
-    m_climbMotors.set(ClimberConstants.kClimberSpeed);
+    m_climbMotor1.set(ClimberConstants.kClimberSpeed);
   }
   public void stop(){
-    m_climbMotors.set(0);
+    m_climbMotor1.set(0);
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    m_climbMotor2.setControl(m_climbMotor2Follower);  
   }
 }
