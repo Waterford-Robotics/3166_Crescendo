5fe6bad9199b9c3b1eb25394db55fb41006b7f1c
lrjr24 <liamreddy2943@gmail.com>
2024-02-12 17:43:36 -0700

    updated to 2024

--- src/main/java/frc/robot/subsystems/AmpTop.java
+++ src/main/java/frc/robot/subsystems/AmpTop.java
@@ -5,19 +5,17 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.AmpConstants;
 
 public class AmpTop extends SubsystemBase {
-  private CANSparkMax m_ampShooter1 = new CANSparkMax(AmpConstants.kAmpShooterID1, MotorType.kBrushless);
-  private CANSparkMax m_ampShooter2 = new CANSparkMax(AmpConstants.kAmpShooterID2, MotorType.kBrushless);
-  private MotorControllerGroup m_shooters = new MotorControllerGroup(m_ampShooter1, m_ampShooter2);
+  private CANSparkMax m_shooters = new CANSparkMax(AmpConstants.kAmpShooterID1, MotorType.kBrushless);
   /** Creates a new AmpShooterSubsystem. */
   public AmpTop() {
-    m_ampShooter2.setInverted(true);
+
   }
 
   public void shoot(){
