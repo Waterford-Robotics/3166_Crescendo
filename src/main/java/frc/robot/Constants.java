ecaa19281784d1c2691c1ba85c0efc18e7b1f7d7
Jet Webster <jetwebster@students.waterfordschool.org>
2024-02-16 16:57:06 -0700

    organised Constants climber

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,29 +3,33 @@ package frc.robot;
 public final class Constants {
 
   // Constants go here...
+  public static final int kDriverControllerPort = 0;
+  public static final int kOperatorControllerPort = 1;
+
   public static final int kLcmotorID = 4;
   public static final int kRcmotorID = 5;
+  
+  public static final int kShaftmotorId = 6;
   
   public static final int kRight1DriveMotorId = 0;
   public static final int kRight2DriveMotorId = 1;
   public static final int kLeft1DriveMotorId = 2;
   public static final int kLeft2DriveMotorId = 3;
-  public static final int kShaftmotorId = 6;
-
-  public static final int kDriverControllerPort = 0;
+  
   public static final int kDriverControllerForwardAxisId = 1;
   public static final int kDriverControllerTurningAxisId = 4;
 
-  public static final int kOperatorControllerPort = 1;
-  public static final int kShootButtonId = 2;
-
   public static final double kMoveSpeed = 0.7;
   public static final double kShootSpeed = 0.7;
+  public static final double kClimbSpeed = 0.7;
 
-  public static final double kClimbSpeed = .7;
-  
-  public static final int kControllerID = 0;
+  public static final int kShootButtonId = 2;
   public static final int kClimbButtonID = 1;
+  
+
+  
+  
+  
   // This class cannot be initalized.
   private Constants() {}
 }
