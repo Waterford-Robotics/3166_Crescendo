f6e1bd9d62407afbdbd049bdb4f8138669d52eae
Ivan Post <ivan.post24@gmail.com>
2024-02-20 16:59:47 -0700

    Add button mapping for aiming and driving

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,13 @@
 
 package frc.robot;
 
+import org.photonvision.PhotonCamera;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
 import frc.robot.Constants.OIConstants;
+import frc.robot.commands.AimAndDriveWithControllerCommand;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -24,6 +27,7 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 public class RobotContainer {
   // The robot's subsystems
   private final Drivetrain m_robotDrive = new Drivetrain();
+  private final PhotonCamera m_camera = new PhotonCamera("Camera_Module_v1");
 
   // The driver's controller
   XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
@@ -61,6 +65,9 @@ public class RobotContainer {
         .whileTrue(new RunCommand(
             () -> m_robotDrive.setX(),
             m_robotDrive));
+
+      new JoystickButton(m_driverController, Button.kLeftBumper.value)
+        .whileTrue(new AimAndDriveWithControllerCommand(m_robotDrive, m_camera, m_driverController, 0));
   }
 
   /**
