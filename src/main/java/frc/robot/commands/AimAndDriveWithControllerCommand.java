c00ed8f8f88268cc9ca3eba1ae953e12bba9f2ef
Ivan Post <ivan.post24@gmail.com>
2024-02-26 16:10:45 -0700

    Remove seen-target method because it cannot work as intended

--- src/main/java/frc/robot/commands/AimAndDriveWithControllerCommand.java
+++ src/main/java/frc/robot/commands/AimAndDriveWithControllerCommand.java
@@ -4,31 +4,38 @@
 
 package frc.robot.commands;
 
-import java.util.Optional;
+import java.util.Objects;
 
 import org.photonvision.PhotonUtils;
-import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.OIConstants;
 import frc.robot.subsystems.Drivetrain;
 
 public class AimAndDriveWithControllerCommand extends Command {
 
-  private static final double kP = 0.01;
-  private static final double kI = 0.00;
-  private static final double kD = 0.00;
+  private static final record AngleDistancePair (double angleErrorDegrees, double distanceToTargetMeters) {
+    AngleDistancePair {
+      Objects.requireNonNull(angleErrorDegrees);
+      Objects.requireNonNull(distanceToTargetMeters);
+    }
+  }
+
+  private static final double kTurningP = 0.01;
+  private static final double kTurningI = 0.00;
+  private static final double kTurningD = 0.00;
 
   private Drivetrain m_drivetrain;
 
   private XboxController m_manualController;
   protected int m_fiducialId;
 
-  private PIDController m_turnController = new PIDController(kP, kI, kD);
+  private PIDController m_turnController = new PIDController(kTurningP, kTurningI, kTurningD);
 
   /** Creates a new AimAndDriveCommand. */
   public AimAndDriveWithControllerCommand(Drivetrain drivetrain, XboxController controller, int targetFiducialId) {
@@ -48,30 +55,24 @@ public class AimAndDriveWithControllerCommand extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double angleErrorDegrees = getError().orElseGet(() -> {
-      Pose2d robotPose = m_drivetrain.getPose();
-      Pose2d targetPose = m_drivetrain.getVisionDataProvider().getFieldLayout().getTagPose(m_fiducialId)
-          .orElseThrow(() -> new IllegalArgumentException("The tag you requested (ID " + m_fiducialId + ") is not on the field"))
-          .toPose2d();
-      return -PhotonUtils.getYawToPose(robotPose, targetPose).getDegrees();
-    });
+    AngleDistancePair error = getError();
     
     m_drivetrain.drive(-MathUtil.applyDeadband(m_manualController.getLeftY(), OIConstants.kDriveDeadband),
                        -MathUtil.applyDeadband(m_manualController.getLeftX(), OIConstants.kDriveDeadband),
-                       m_turnController.calculate(angleErrorDegrees),
+                       m_turnController.calculate(error.angleErrorDegrees()),
                        true, true);
+
+    SmartDashboard.putNumber("Angle error from target (degrees)", error.angleErrorDegrees());
+    SmartDashboard.putNumber("Distance from target (meters)", error.distanceToTargetMeters());
   }
 
-  private Optional<Double> getError() {
-    Optional<PhotonTrackedTarget> target = m_drivetrain.getVisionDataProvider().getTargets().stream()
-      .filter(x -> x.getFiducialId() == m_fiducialId)
-      .findFirst();
-    
-    if (target.isPresent()) {
-      return Optional.of(target.get().getYaw());
-    } else {
-      return Optional.empty();
-    }
+  public AngleDistancePair getError() {
+    Pose2d robotPose = m_drivetrain.getPose();
+    Pose2d targetPose = m_drivetrain.getVisionDataProvider().getFieldLayout().getTagPose(m_fiducialId)
+        .orElseThrow(() -> new IllegalArgumentException("The tag you requested (ID " + m_fiducialId + ") is not on the field"))
+        .toPose2d();
+    return new AngleDistancePair(-PhotonUtils.getYawToPose(robotPose, targetPose).getDegrees(),
+                                  PhotonUtils.getDistanceToPose(robotPose, targetPose));
   }
 
   // Called once the command ends or is interrupted.
