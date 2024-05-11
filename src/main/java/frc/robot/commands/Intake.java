eb8a98f48b4c2b73e88ded35fbedacaa0a8c5d5c
lrjr24 <liamreddy2943@gmail.com>
2024-05-11 10:45:48 -0600

    added paths + autos + intake command

---
+++ src/main/java/frc/robot/commands/Intake.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.SpeakerTop;
+
+public class Intake extends Command {
+  private final SpeakerTop m_speakerTop = new SpeakerTop();
+  Timer timer = new Timer();
+  public Intake() {
+    addRequirements(m_speakerTop);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    timer.start();
+    timer.reset();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (timer.get()<1){
+      m_speakerTop.intake();
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
