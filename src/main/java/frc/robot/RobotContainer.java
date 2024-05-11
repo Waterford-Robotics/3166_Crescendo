2cf00dde65cfc881a9d8a8f093adbf24e975a9da
lrjr24 <liamreddy2943@gmail.com>
2024-05-11 11:31:27 -0600

    added autos to chooser

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -67,6 +67,8 @@ public class RobotContainer {
     m_chooser.addOption("Far to Amp Out In Auto", m_farAmpOutInAuto);
     m_chooser.addOption("Shoot Only Auto", m_shootOnlyAuto);
     m_chooser.addOption("Straight Line",m_robotDrive.getAuto("Straight Line"));
+    m_chooser.addOption("Straight Line",m_robotDrive.getAuto("Two Note Center to Right Wall"));
+    m_chooser.addOption("Straight Line",m_robotDrive.getAuto("Two Note Right Edge to Right Wall"));
     m_fieldrelativechooser.setDefaultOption("Field Relative", true);
     m_fieldrelativechooser.addOption("Robot Relative", false);
     m_timeDelayChooser.setDefaultOption("0 seconds", 0.0);
